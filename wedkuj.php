c10f62486e888da3b644d36c0561c25dc2158c8f
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-04-03 11:26:56 +0200

    Wszystho bez skalowania grafiki i php

---
+++ wedkuj.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Wędkujemy</title>
+    <link rel="stylesheet" href="styl1.css">
+</head>
+
+<body>
+    <header>
+        <h1>Portal dla Wędkarzy</h1>
+    </header>
+    <div id="left">
+        <h2>Ryby drapieżne naszych wód</h2>
+        <ul></ul>
+    </div>
+    <div id="right">
+        <img src="ryba1.jpg" alt="Sum">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </div>
+    <footer>
+        <p>Stronę wykonał: Stanisław Fiedoruk 4J</p>
+    </footer>
+</body>
+
+</html>
